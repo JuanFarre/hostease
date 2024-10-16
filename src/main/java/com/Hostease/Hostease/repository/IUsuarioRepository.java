14fc67059bdb819e5e2c9b4185848ab04304eb82
JuanFarre <juanfarre99@gmail.com>
2024-10-16 10:38:14 -0300

    empezando a implementar seguridad

--- src/main/java/com/Hostease/Hostease/repository/IUsuarioRepository.java
+++ src/main/java/com/Hostease/Hostease/repository/IUsuarioRepository.java
@@ -4,9 +4,12 @@ import com.Hostease.Hostease.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    public Optional<Usuario> findByUsername(String username);
 
 
 }
---
+++ src/main/java/com/Hostease/Hostease/security/config/SecurityBeansInjector.java
@@ -0,0 +1,52 @@
+package com.Hostease.Hostease.security.config;
+
+import com.Hostease.Hostease.model.Usuario;
+import com.Hostease.Hostease.repository.ITipoUsuarioRepository;
+import com.Hostease.Hostease.repository.IUsuarioRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityBeansInjector {
+
+    @Autowired
+    AuthenticationConfiguration authenticationConfiguration;
+
+    @Autowired
+    private IUsuarioRepository usuarioRepository;
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationStrategy = new DaoAuthenticationProvider();
+        authenticationStrategy.setPasswordEncoder(passwordEncoder());
+        authenticationStrategy.setUserDetailsService(userDetailsService());
+
+        return authenticationStrategy;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return(username) ->{
+        return usuarioRepository.findByUsername(username)
+                .orElseThrow(()->new EntityNotFoundException("No existe el usuario "+ username));
+    }
+
+
+}
